1efc08033cd6f06f59805a2ae2c68d650edab0b5
Charles Sprayberry <me@cspray.io>
2019-08-24 17:08:26 -0400

    add new exceptions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v1.1.0 - 2019-08-24
+
+- Added CircularDependencyException
+- Added DependencyInjectionException
+- Added InvalidStateException
+
 ## v1.0.0 - 2017-05-13
 
 - Added NotFoundException
---
+++ src/DependencyInjectionException.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Cspray\Labrador\Exception;
+
+/**
+ *
+ * @package Cspray\Labrador\Exception
+ * @license See LICENSE in source root
+ */
+class DependencyInjectionException extends Exception {
+
+}
